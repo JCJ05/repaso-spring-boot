208e3af38d29a46b0335fc0a0e7e43685412f2ea
JCJ05 <julitoaguerorengifo@hotmail.com>
2023-08-14 10:19:27 -0500

    Diferentes formas de mandar datos a la vista

--- spring-web-mvc/src/main/java/com/spring/mvc/springwebmvc/IndexController.java
+++ spring-web-mvc/src/main/java/com/spring/mvc/springwebmvc/IndexController.java
@@ -1,17 +1,24 @@
 package com.spring.mvc.springwebmvc;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     
     @RequestMapping(path = {"", "/", "/index"} , method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index(Model model ,ModelMap modelMap , Map<String , Object> map , ModelAndView modelAndView) {
 
         model.addAttribute("mensaje", "Hola Spring MVC 5!");
+        modelMap.addAttribute("mensaje2", "Hola Spring MVC 5! con ModelMap");
+        map.put("mensaje3", "Hola Spring MVC 5! con Map");
+        modelAndView.addObject("mensaje4", "Hola Spring MVC 5! con ModelAndView");
 
         return "index";
     }
