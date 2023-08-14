f1f9e7a3015df51ee8f5118fdb3ee354685f12f5
JCJ05 <julitoaguerorengifo@hotmail.com>
2023-08-14 10:39:06 -0500

    Trabajar con th:each

--- spring-web-mvc/src/main/java/com/spring/mvc/springwebmvc/controller/IndexController.java
+++ spring-web-mvc/src/main/java/com/spring/mvc/springwebmvc/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.spring.mvc.springwebmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -36,5 +38,20 @@ public class IndexController {
         model.addAttribute("usuario", usuario);
         
         return "perfil";
+    }
+
+    @RequestMapping(path = "/listar")
+    public String listar(Model model) {
+        
+        List<Usuario> usuarios = new ArrayList<>();
+
+        usuarios.add(new Usuario("John", "Doe", "jhon@gmail.com"));
+        usuarios.add(new Usuario("Jane", "Doe", "jane@gmail.com"));
+        usuarios.add(new Usuario("Max", "Doe", "max@gmail.com"));
+
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", usuarios);
+
+        return "listar";
     }
 }
